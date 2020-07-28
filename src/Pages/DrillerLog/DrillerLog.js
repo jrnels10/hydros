4ea8f95d4629682b8440d6473f70ae56179488e2
Ian Sutherland <isutherland@azwater.gov>
2020-07-28 16:18:29 -0700

    fixed oversized div causing overflow

--- src/Pages/DrillerLog/DrillerLog.js
+++ src/Pages/DrillerLog/DrillerLog.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react'
 import LogSearch from './LogSearch'
+import { RibbonContainer } from '../../components/Containers/Ribbons'
+import { NavButton } from '../../components/Buttons/NavButtons'
 
 export default class DrillerLog extends Component {
     render() {
         return (
             <div id="LogContainer" className="h-100">
-                <div className="container h-100 p-3">
-                <div className="row w-100 h-100 align-items-center">
-                    <div className="col-md-8 offset-md-2">
-                        <LogSearch></LogSearch>
+                <RibbonContainer>
+                    <div className="button__container float-right">
+                        <NavButton to='/logs/search'>
+                            <button onClick={() => this.setState({logSearch: true})}>Search</button>
+                        </NavButton>
                     </div>
-                </div>
+                </RibbonContainer>
+                <div className="container p-3">
+                    <div className="row w-100 h-100 align-items-center">
+                        <div className="col-md-8 offset-md-2">
+                            <LogSearch></LogSearch>
+                        </div>
+                    </div>
                 </div>
             </div>
         )

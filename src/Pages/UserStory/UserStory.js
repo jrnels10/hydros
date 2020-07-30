d8e759c670003ada1a99073298b73beca5225e02
Ian Sutherland <isutherland@azwater.gov>
2020-07-29 17:34:45 -0700

    css looks better and form is functioning

--- src/Pages/UserStory/UserStory.js
+++ src/Pages/UserStory/UserStory.js
@@ -2,52 +2,63 @@ import React, { Component } from 'react'
 import './Story.css';
 
 export default class UserStory extends Component {
+    constructor(props){
+        super(props);
+        this.state = {role: "", action: "", benefit: ""};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        let prop = event.target.name;//name attribute of input element
+        if(prop === "userRole"){
+            this.setState({role: event.target.value});
+        }
+        if(prop === "userAction"){
+            this.setState({action: event.target.value});
+        }
+        if(prop === "userBenefit"){
+            this.setState({benefit: event.target.value});
+        }
+        console.log(event.target.name)
+    }
+
+    handleSubmit(event) {
+        alert(`role:${this.state.role}\naction:${this.state.action}\nbenefit:${this.state.benefit}`);
+        this.setState({role:'', action:'', benefit:''});
+        event.preventDefault();
+        
+    }
+
     render() {
         return (
-            <div className="container-fluid p-3" id="StoryContainer">
-                <div className="container text-center">
-                <div className="row w-100 align-items-center">
-                    <div className="col-md-12">
-                        <form action="">
-                            <div className="col-md-4">
-                            <div className="form-group">
-                                <div className="row">
-                                    <div className="col-md-6">
+            <div id="StoryContainer">
+                <div className="container h-100">
+                    <div className="row h-100 align-items-center">
+                        <div className="col-md-8 offset-md-2">
+                        <div className="jumbotron">
+                        <h4>See a chance to make this app better?</h4>
+                        <h4>Submit a User Story for the Dev team!</h4>
+                        <h6>The structure of a user story is laid out below</h6>
+                            <form onSubmit={this.handleSubmit}>
+                                <div className="form-group">
                                     <label htmlFor="userRole">As a...</label>
-                                    </div>
-                                    <div className="col-md-6">
-                                    <input type="text" name="userRole" placeholder="<role>" className="form-control"/>
-                                    </div>
+                                    <input type="text" value={this.state.user} name="userRole" placeholder="<user>" onChange={this.handleChange}/>
                                 </div>
-                            </div>
-                            </div>
-                            <div className="col-md-4 offset-md-1">
-                            <div className="form-group">
-                                <div className="row">
-                                    <div className="col-md-6">
+                                <div className="form-group">
                                     <label htmlFor="userAction">I want to...</label>
-                                    </div>
-                                    <div className="col-md-6">
-                                    <input type="text" name="userAction" placeholder="<action>" className="form-control"/>
-                                    </div>
+                                    <input type="text" name="userAction" placeholder="<action>" onChange={this.handleChange}/>
                                 </div>
-                            </div>
-                            </div>
-                            <div className="col-md-4 offset-md-2">
-                            <div className="form-group">
-                                <div className="row">
-                                    <div className="col-md-6">
+                                <div className="form-group">
                                     <label htmlFor="userBenefit">so that...</label>
-                                    </div>
-                                    <div className="col-md-6">
-                                    <input type="text" name="userBenefit" placeholder="<benefit>" className="form-control"/>
-                                    </div>
+                                    <input type="text" name="userBenefit" placeholder="<benefit>" onChange={this.handleChange}/>
                                 </div>
-                            </div>
-                            </div>
-                        </form>
+                                <button className="btn btn-primary btn-lg">Submit User Story</button>
+                            </form>
+                        </div>
+                        </div>
                     </div>
-                </div>
                 </div>
             </div>
         )
